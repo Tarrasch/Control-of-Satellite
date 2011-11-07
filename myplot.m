7a097fe400216fe8dbccd32d36110b1d6c305cdc
Arash Rouhani <miffoljud@gmail.com>
2011-11-07 17:41:42 +0100

    Fixed all bugs :)

---
+++ myplot.m
@@ -0,0 +1,13 @@
+function [ ] = myplot( Kp, Kd )
+%MYPLOT Plot our system step response given Kp and Kd
+
+s = tf('s');
+d = 4;
+J = 5;
+num = d/J*Kp;
+den = s^2 + d/J*Kd*s + d/J*Kp;
+G = num/den;
+step(G);
+
+end
+
---
+++ plotall.m
@@ -0,0 +1,6 @@
+Kp = 5/4;
+Kd = 5/4;
+
+myplot(Kp, Kd); hold on;
+myplot(2*Kp, Kd); hold on;
+myplot(Kp, 2*Kd); waitforbuttonpress;
